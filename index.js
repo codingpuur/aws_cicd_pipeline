b7ef173b9e85c7aa8a9c58f13587eaf908d40ba5
codingpuur <chaudharysc73342@gmail.com>
2024-12-30 11:50:14 +0530

    test2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 
 app.use('/',(req,res)=>{
-   res.send('shubham is learning ci/cd this is testing')
+   res.send('shubham is testing ci/cd')
 
 })
 app.listen(4000,()=>{
